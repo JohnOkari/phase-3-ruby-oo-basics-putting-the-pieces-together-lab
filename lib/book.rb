b8b4daaaf01eeecf2c1a429aa68d8b71acdad7b5
JohnOkari <johnnokari@gmail.com>
2023-02-09 06:45:51 +0300

    Done with lab

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+class Book
 
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
+
+    def initialize(title)
+        @title = title
+        # @author = author
+        # @page_count = page_count
+        # @genre = genre
+        # @current_page = 0
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+    
+end
 
